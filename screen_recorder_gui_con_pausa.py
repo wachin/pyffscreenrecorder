9d4f85ee9d02cea9c6e4f2a153b0e66278bb1974
wachin <wachin.id@gmail.com>
2024-09-25 22:55:43 -0500

    Añado la opción de pausa

--- screen_recorder_gui_con_pausa.py
+++ screen_recorder_gui_con_pausa.py
@@ -15,19 +15,32 @@ class ScreenRecorderGUI:
         self.master = master
         master.title("Grabador de Pantalla")
 
+        # Botón para iniciar grabación
         self.start_button = tk.Button(master, text="Iniciar Grabación", command=self.start_recording)
         self.start_button.pack()
 
+        # Botón para pausar grabación
+        self.pause_button = tk.Button(master, text="Pausar Grabación", command=self.pause_recording, state=tk.DISABLED)
+        self.pause_button.pack()
+
+        # Botón para reanudar grabación
+        self.resume_button = tk.Button(master, text="Reanudar Grabación", command=self.resume_recording, state=tk.DISABLED)
+        self.resume_button.pack()
+
+        # Botón para detener grabación
         self.stop_button = tk.Button(master, text="Detener Grabación", command=self.stop_recording, state=tk.DISABLED)
         self.stop_button.pack()
 
+        # Área de texto para mostrar la salida de ffmpeg
         self.output_text = tk.Text(master, height=10, width=50)
         self.output_text.pack()
 
+        # Variable para manejar el proceso de grabación
         self.process = None
 
     def start_recording(self):
         self.start_button.config(state=tk.DISABLED)
+        self.pause_button.config(state=tk.NORMAL)
         self.stop_button.config(state=tk.NORMAL)
         self.output_text.delete(1.0, tk.END)
 
@@ -44,9 +57,25 @@ class ScreenRecorderGUI:
 
             self.process.wait()
             self.start_button.config(state=tk.NORMAL)
+            self.pause_button.config(state=tk.DISABLED)
+            self.resume_button.config(state=tk.DISABLED)
             self.stop_button.config(state=tk.DISABLED)
 
         threading.Thread(target=run_ffmpeg, daemon=True).start()
+
+    def pause_recording(self):
+        if self.process:
+            # Enviar la señal SIGSTOP para pausar la grabación
+            os.killpg(os.getpgid(self.process.pid), signal.SIGSTOP)
+            self.pause_button.config(state=tk.DISABLED)
+            self.resume_button.config(state=tk.NORMAL)
+
+    def resume_recording(self):
+        if self.process:
+            # Enviar la señal SIGCONT para reanudar la grabación
+            os.killpg(os.getpgid(self.process.pid), signal.SIGCONT)
+            self.resume_button.config(state=tk.DISABLED)
+            self.pause_button.config(state=tk.NORMAL)
 
     def stop_recording(self):
         if self.process:
@@ -59,6 +88,8 @@ class ScreenRecorderGUI:
                 pass
 
         self.start_button.config(state=tk.NORMAL)
+        self.pause_button.config(state=tk.DISABLED)
+        self.resume_button.config(state=tk.DISABLED)
         self.stop_button.config(state=tk.DISABLED)
 
 root = tk.Tk()
--- src/ffscreenrecord.sh
+++ src/ffscreenrecord.sh
@@ -2,14 +2,15 @@
 
 set -x
 
-# Obtener las dimensiones de la pantalla
+# Obtener las dimensiones de la pantalla con xdpyinfo del paquete x11-utils
 SCREEN_WIDTH=$(xdpyinfo | awk '/dimensions/{print $2}' | cut -d'x' -f1)
 SCREEN_HEIGHT=$(xdpyinfo | awk '/dimensions/{print $2}' | cut -d'x' -f2)
 
-# Calcular las coordenadas para centrar la ventana de grabación
+# Calcula las coordenadas para centrar la ventana de grabación de 854x480 en tu pantalla.
 X_OFFSET=$((($SCREEN_WIDTH - 854) / 2))
 Y_OFFSET=$((($SCREEN_HEIGHT - 480) / 2))
 
+# Usa estas coordenadas calculadas en el comando -i de FFmpeg.
 ffmpeg -follow_mouse 50 -show_region 1 -video_size 854x480 -framerate 30 -f x11grab -i :0.0+$X_OFFSET,$Y_OFFSET \
        -f alsa -ac 2 -i default \
        -c:v libx264rgb -crf 0 -preset ultrafast \
